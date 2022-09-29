b41dac2a26d1da4da1b135169d95f19bd0d31007
Bilel Grissa <bilel.grissa@econocom.com>
2022-09-29 19:36:09 +0200

    First Step Done.

--- src/coffeeMachine/Application.java
+++ src/coffeeMachine/Application.java
@@ -4,8 +4,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		Maker maker = new MakerImpl();
-		String res = maker.transformer(new Order(Drink.THE, 2));
-		System.out.println(res);
+		System.out.println(maker.transformer(new Order(Drink.THE, 2)));
+		System.out.println(maker.transformer(new Order(Drink.COFFEE, 1)));
+		System.out.println(maker.transformer(new Order(Drink.CHOCOLATE, 3)));
+		System.out.println(maker.transformer(new Order(Drink.THE, 0)));
 	}
 
 }
